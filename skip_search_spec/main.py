1e7efbc5e8fc2e757b07459ab6253b6793c3b9bc
Hugo Olsson <hugo.contact01@gmail.com>
2026-05-02 22:39:12 +0200

    Testing with more layers in Qwen3

--- skip_search_spec/main.py
+++ skip_search_spec/main.py
@@ -30,11 +30,11 @@ def main() -> None:
         from skip_search_spec.experiments.dataset_mix import get_dataset_mix
         from skip_search_spec.training.train_skipping_layers import train_skipping_layers
 
-        number_of_windows = 50000
+        number_of_windows = 20000
         num_epochs = 1 # Ensure never get scores on data it has seen
 
         models = ["Qwen/Qwen3-4B"]
-        active_start_end_lengths = [(4, 4)]
+        active_start_end_lengths = [(9, 9)]
 
         # SINGLE LAYER AT START
         print("Version: 1.6")
