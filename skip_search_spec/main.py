bab798c7c1ac8e25a2d9150c4a4f60615738981c
Hugo Olsson <hugo.contact01@gmail.com>
2026-05-06 20:09:42 +0200

    Using "mistralai/Mistral-7B-Instruct-v0.3"

--- skip_search_spec/main.py
+++ skip_search_spec/main.py
@@ -34,10 +34,10 @@ def main() -> None:
         from skip_search_spec.experiments.dataset_mix import get_dataset_mix
         from skip_search_spec.training.train_skipping_layers import train_skipping_layers
 
-        number_of_windows = 30_000
+        number_of_windows = 50_000
         num_epochs = 1 # Ensure never get scores on data it has seen
 
-        models = ["Qwen/Qwen3-4B-Instruct-2507"]
+        models = ["mistralai/Mistral-7B-Instruct-v0.3"]
         active_start_end_lengths = [(2, 2)]
 
         # SINGLE LAYER AT START
@@ -52,7 +52,7 @@ def main() -> None:
                     dataset_mix=get_dataset_mix(number_of_windows),
                     context_len=256,
                     num_windows_to_use=number_of_windows,
-                    batch_size=40,
+                    batch_size=10,
                     active_start_layers=active_start_layers, 
                     active_end_layers=active_end_layers,
                     num_epochs=num_epochs,
