347f0e4e1c5c5ebdf46ea508955a9a572602e6e6
Hugo Olsson <hugo.contact01@gmail.com>
2026-05-06 18:24:50 +0200

    Training Llama3.1-8B-Instruct

--- skip_search_spec/main.py
+++ skip_search_spec/main.py
@@ -34,10 +34,10 @@ def main() -> None:
         from skip_search_spec.experiments.dataset_mix import get_dataset_mix
         from skip_search_spec.training.train_skipping_layers import train_skipping_layers
 
-        number_of_windows = 50_000
+        number_of_windows = 100_000
         num_epochs = 1 # Ensure never get scores on data it has seen
 
-        models = ["meta-llama/Llama-3.2-3B-Instruct"]
+        models = ["meta-llama/Llama-3.1-8B-Instruct"]
         active_start_end_lengths = [(4, 4)]
 
         # SINGLE LAYER AT START
@@ -50,7 +50,7 @@ def main() -> None:
                 train_skipping_layers(
                     model_name=model,
                     dataset_mix=get_dataset_mix(number_of_windows),
-                    context_len=120,
+                    context_len=256,
                     num_windows_to_use=number_of_windows,
                     batch_size=30,
                     active_start_layers=active_start_layers, 
@@ -63,7 +63,7 @@ def main() -> None:
                     ce_loss_weight=1.0,
                     checkpoint_every_steps=None,
                     log_every=100,
-                    num_draft_sections=10,
+                    num_draft_sections=5,
                     reference_hidden_source="final"
                 )
 
