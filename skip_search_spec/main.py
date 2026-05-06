86d458c9c68d7345f6b2308ec5a3dc07a8279884
Hugo Olsson <hugo.contact01@gmail.com>
2026-05-06 15:23:03 +0200

    Fewer windows

--- skip_search_spec/main.py
+++ skip_search_spec/main.py
@@ -34,7 +34,7 @@ def main() -> None:
         from skip_search_spec.experiments.dataset_mix import get_dataset_mix
         from skip_search_spec.training.train_skipping_layers import train_skipping_layers
 
-        number_of_windows = 100_000
+        number_of_windows = 20_000
         num_epochs = 1 # Ensure never get scores on data it has seen
 
         models = ["meta-llama/Llama-3.2-3B-Instruct"]
